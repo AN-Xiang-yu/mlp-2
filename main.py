766322e0a8816c03103d3a5a363b9cacf569a1e5
Katia <katia.abbaoui@efrei.net>
2023-11-29 15:03:44 +0100

    modif get genre api

--- main.py
+++ main.py
@@ -4,13 +4,20 @@ import uvicorn
 from fastapi import FastAPI
 
 # Internal modules
-from modules.commun import get_movies
+from src.commun import get_movies
+from src.commun import get_genre
 
 
 app = FastAPI()
 
+app = FastAPI()
+# use the api read_root
+@app.get("/get_movies_genre")
+def display_movies_genre():
+    movies_genre = get_genre()
+    movies_genre.to_json(orient='records')
+    return movies_genre
 
 if __name__ == '__main__':
-    app = FastAPI()
-    # use the api read_root
+    
     uvicorn.run(app, host="0.0.0.0", port=8000)
