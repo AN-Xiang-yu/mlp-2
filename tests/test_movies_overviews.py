0c33b52cc089ad9abf91ee6333395b37d1366202
Xiangyu AN <xiangyu.an@efrei.net>
2023-12-02 21:36:33 +0100

    mock test

--- tests/test_movies_overviews.py
+++ tests/test_movies_overviews.py
@@ -1,8 +1,10 @@
 # Installed packages
 import pandas as pd
+import pytest
 
 # Internal modules
 from src.movies_overviews import get_movies_overviews
+from main import display_movies_overviews
 
 
 def init_movies_overviews() -> str:
@@ -50,3 +52,29 @@ def test_type_get_movies_overviews():
     # ensure that every entry in the 'overview' column is a string
     assert all(isinstance(item, str)
                for item in movies_overviews['overview']), "Every item in the 'overview' column should be a string."
+
+
+def test_display_movies_overviews_with_mocks(mocker):
+    """Test the function display_movies_overviews.
+        Args:
+            mocker: The mocker object of pytest.
+    """
+    # initialisation
+    data_mocked = pd.DataFrame([
+        {'overview': "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth. "},
+        {'overview': "Plagued by strange memories, Neo's life takes an unexpected turn when he finds himself back inside the Matrix. "}
+    ])
+
+    # simulate the function
+    mock_get_movies_overviews = mocker.patch("main.get_movies_overviews",
+                                             return_value=data_mocked)
+
+    # use the mocked function
+    response = display_movies_overviews("The Matrix")
+
+    # assert that the get_movies_overviews was called with the right argument
+    mock_get_movies_overviews.assert_called_once_with("The Matrix")
+
+    # verify that the mocked data is the same as the result
+    assert response == data_mocked.to_json(
+        orient='records'), "The mocked data should be the same as the result."
